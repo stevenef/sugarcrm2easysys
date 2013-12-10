12f99b7ea0ba243003177169a4e7fc2f5b338eda
Steven Helmis <steven.helmis@scitotec.de>
2013-12-10 21:48:13 +0100

    weiterentwicklung und die einzelnen Kontakte den Firmen zugewiesen

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ date_default_timezone_set("GMT");
 
 include 'lib/Logger.php';
 include 'lib/Mysql.php';
+include 'lib/Zend/Debug.php';
 
 class Csv {
 	public function fwritecsv($handle, $fields, $delimiter = ',', $enclosure = '"') {
@@ -11,24 +12,37 @@ class Csv {
 	    if (!is_array($fields)) {
 	        return false;
 	    }
+	    
 	    # Walk through the data array
 	    for ($i = 0, $n = count($fields); $i < $n; $i ++) {
 	        # Only 'correct' non-numeric values
-	        if (!is_numeric($fields[$i])) {
+
+	        $fields = $this->replaceLineBreaks($fields);
+
+	        //if (!is_numeric($fields[$i])) {
 	            # Duplicate in-value $enclusure's and put the value in $enclosure's
-	            $fields[$i] = $enclosure . str_replace($enclosure, $enclosure . $enclosure, $fields[$i]) . $enclosure;
-	        }
+	            // $fields[$i] = $enclosure . str_replace($enclosure, $enclosure . $enclosure, $fields[$i]) . $enclosure;
+	        //}
 	        # If $delimiter is a dot (.), also correct numeric values
-	        if (($delimiter == '.') && (is_numeric($fields[$i]))) {
-	            # Put the value in $enclosure's
-	            $fields[$i] = $enclosure . $fields[$i] . $enclosure;
-	        }
+	        // if (($delimiter == '.') && (is_numeric($fields[$i]))) {
+	        //     # Put the value in $enclosure's
+	        //     $fields[$i] = $enclosure . $fields[$i] . $enclosure;
+	        // }
 	    }
+
+	    $fields = $this->replaceLineBreaks($fields);
+
 	    # Combine the data array with $delimiter and write it to the file
-	    $line = implode($delimiter, $fields) . "\n";
+	    //$line = implode($delimiter, $fields) . "\n";
+	    $line = implode($delimiter, $fields);
 	    fwrite($handle, $line);
 	    # Return the length of the written data
 	    return strlen($line);
+	}
+
+	public function replaceLineBreaks($str, $replace = '') {
+		$chars = array("\n", "\r", "chr(13)",  "\t", "\0", "\x0B");
+		return str_replace($chars, $replace, $str);
 	}
 }
 
@@ -59,39 +73,39 @@ $header = array(
 	'Ansprechpartner',
 	'Sprache'
 );
+	
 
-$key = 1;
+for ($key=1;$key<11;$key++) {
+	$kontaktperson = array(
+		'Kontaktperson '.$key.' Kategorie',
+		'Kontaktperson '.$key.' Branche',
+		'Kontaktperson '.$key.' Anrede',
+		'Kontaktperson '.$key.' Titel',
+		'Kontaktperson '.$key.' Nachname',
+		'Kontaktperson '.$key.' Vorname',
+		'Kontaktperson '.$key.' Adresse',
+		'Kontaktperson '.$key.' PLZ',
+		'Kontaktperson '.$key.' Ort',
+		'Kontaktperson '.$key.' Land',
+		'Kontaktperson '.$key.' Email',
+		'Kontaktperson '.$key.' Email 2',
+		'Kontaktperson '.$key.' Telefon',
+		'Kontaktperson '.$key.' Telefon 2',
+		'Kontaktperson '.$key.' Mobile',
+		'Kontaktperson '.$key.' Fax',
+		'Kontaktperson '.$key.' Website',
+		'Kontaktperson '.$key.' Skype',
+		'Kontaktperson '.$key.' Funktion',
+		'Kontaktperson '.$key.' Anredeform',
+		'Kontaktperson '.$key.' Geburtstag',
+		'Kontaktperson '.$key.' Bemerkungen',
+		'Kontaktperson '.$key.' Ansprechpartner',
+		'Kontaktperson '.$key.' Sprache'
+	);
 
-$kontaktperson = array(
-	'Kontaktperson '.$key.' Kategorie',
-	'Kontaktperson '.$key.' Branche',
-	'Kontaktperson '.$key.' Anrede',
-	'Kontaktperson '.$key.' Titel',
-	'Kontaktperson '.$key.' Nachname',
-	'Kontaktperson '.$key.' Vorname',
-	'Kontaktperson '.$key.' Adresse',
-	'Kontaktperson '.$key.' PLZ',
-	'Kontaktperson '.$key.' Ort',
-	'Kontaktperson '.$key.' Land',
-	'Kontaktperson '.$key.' Email',
-	'Kontaktperson '.$key.' Email 2',
-	'Kontaktperson '.$key.' Telefon',
-	'Kontaktperson '.$key.' Telefon 2',
-	'Kontaktperson '.$key.' Mobile',
-	'Kontaktperson '.$key.' Fax',
-	'Kontaktperson '.$key.' Website',
-	'Kontaktperson '.$key.' Skype',
-	'Kontaktperson '.$key.' Funktion',
-	'Kontaktperson '.$key.' Anredeform',
-	'Kontaktperson '.$key.' Geburtstag',
-	'Kontaktperson '.$key.' Bemerkungen',
-	'Kontaktperson '.$key.' Ansprechpartner',
-	'Kontaktperson '.$key.' Sprache'
-);
-
-
-foreach ($kontaktperson as $value) {
-	array_push($header,$value);
+	foreach ($kontaktperson as $value) {
+		array_push($header,$value);
+	}
 }
 
 
@@ -106,17 +120,40 @@ $log = new Logger();
 // $log->warn("Warnung");
 // $log->err("Fehler");
 
-
 $db = new Mysql();
 
-
-$arrBvDb = array();
-$qry = $db->do_query("SELECT * FROM users");
-var_dump($qry);
+$arrAccounts = $db->getAccounts();
 
 
 
+foreach ($arrAccounts as $key => $value) {
+	// schreibe die Firma in die CSV
+	$csv->fwritecsv($fp,$value,";");
+	// schau ob es eine Verknüpfungen gibt
+	$arrAccountsContacts = $db->getContactIdFromAccountName($value['Name1']);
+	$anz = count($arrAccountsContacts);	
+	// wenn es Verknüpfungen gibt
+	if ($anz > 0) {
+		fwrite($fp, ";");
+		foreach ($arrAccountsContacts as $key => $contact) {
+			// hole den Contact
+			$arrContact = $db->getContactsById($contact['contact_id']);
+			//Zend_Debug::dump($arrContact);
+			$csv->fwritecsv($fp,$arrContact[0],";");
+			fwrite($fp, ";");
+		}
+		
+	}
+	fwrite($fp, "\n");
+}
 
+//Zend_Debug::dump($arrAccountsContacts);
+
+
+
+// foreach ($arrAccounts as $key => $value) {
+// 	$csv->fwritecsv($fp,$value,";");
+// }
 
 
 
--- lib/Mysql.php
+++ lib/Mysql.php
@@ -17,12 +17,7 @@ class Mysql {
 		if ($mysqli->connect_errno) {
 		    printf("Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error);
 		}
-		/* change character set to utf8 */
-		// if (!$mysqli->set_charset("utf8")) {
-		//     printf("Error loading character set utf8: " . $mysqli->error, TRUE);
-		// } else {
-		//     printf("Current character set: " . $mysqli->character_set_name(), TRUE);
-		// }
+
 		$this->_mysqli = $mysqli;
 	}
 
@@ -40,11 +35,60 @@ class Mysql {
 	    $arrData = array();
 	    while (($row = mysqli_fetch_assoc($res))) {
 		    if (count($row) > 0) {
-		        $arrData = $row;
+		        array_push($arrData,$row);
 		    }
 		}
 	    return $arrData;
 	}
+
+
+
+
+	public function getAccounts() {
+		$query1 = 'SELECT "" AS Ketegorie, "" AS Branche, "Firma" AS Kontaktart, LEFT(a.name,80) AS Name1, "" AS Name2, "" AS Anrede, "" AS Titel, 
+		a.billing_address_street AS Adresse, a.billing_address_postalcode AS PLZ, a.billing_address_city AS Ort, 
+		a.billing_address_country AS Land, c.email_address AS Email, "" AS Email2, a.phone_office AS Telefon, a.phone_alternate AS Telefon2,
+		"" AS Mobile, TRIM(a.phone_fax) AS Fax, a.website AS Webseite, "" AS Skype, "" AS Anredeform, "" AS Geburtstag, a.description AS Bemerkung,
+		"steven" AS Ansprechpartner, "" AS Sprache
+		FROM accounts AS a
+		LEFT JOIN email_addr_bean_rel AS b ON a.id = b.bean_id 
+		LEFT JOIN email_addresses AS c ON b.email_address_id = c.id
+		GROUP BY a.name;';
+
+		$arrAccounts = $this->do_query($query1);
+		return $arrAccounts;
+	}
+
+	public function getContactsById($id) {
+		$query = 'SELECT "" AS Ketegorie, "" AS Branche, d.salutation AS Anrede, "" AS Titel,  d.last_name AS Nachname, d.first_name AS Vorname,
+		d.primary_address_street AS Adresse, d.primary_address_postalcode AS PLZ, d.primary_address_city AS Ort, d.primary_address_country AS Land,
+		f.email_address AS Email, "" AS Email2, d.phone_home AS Telefon, d.phone_other AS Telefon2, d.phone_mobile AS Mobile, d.phone_fax AS Fax,
+		"" AS Webseite, "" AS Skype, d.title AS Funktion, "" AS Anredeform, d.birthdate AS Geburtstag, d.description AS Bemerkungen,
+		"" AS Ansprechpartner, "" AS Sprache 
+		FROM contacts AS d
+		LEFT JOIN email_addr_bean_rel AS e ON d.id = e.bean_id 
+		LEFT JOIN email_addresses AS f ON e.email_address_id = f.id
+		where d.`id` = "' . $id .'"';
+
+		$arrContact = $this->do_query($query);
+		return $arrContact;
+	}
+
+
+	public function getContactIdFromAccountName($name){
+		$query = 'select a.`id` as account_id, a.`name`,c.`id` as contact_id, c.`first_name`, c.`last_name`
+		from accounts as a
+		join accounts_contacts as b on a.id = b.`account_id`
+		join contacts as c on b.`contact_id` = c.`id`
+		where a.`name` like "'.$name.'"
+		group by c.`first_name`,c.`last_name`
+		;';
+
+		$arrData = $this->do_query($query);
+		return $arrData;
+	}
+
+
 
 }
 
